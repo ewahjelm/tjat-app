2a0d3e4dffc0c2c7649ff0a5325dff8caa6ba219
Ewa <ewahjelm84@gmail.com>
2025-09-18 14:48:46 +0200

    Initial commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Card = () => {
+
+return(
+<Card></Card>
+);
+
+export default Card;}
---
+++ src/components/Card/Card.stories.jsx
@@ -0,0 +1,10 @@
+import { React} from 'react'
+import { Card } from '.'
+
+export default {
+	title: 'Components/Card',
+	component: Card,
+}
+
+export const Default = {
+}
