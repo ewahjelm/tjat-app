396d62a2817fc5626b4ea8776071e2367edfea2f
Ewa <ewahjelm84@gmail.com>
2025-09-20 21:35:48 +0200

    Chat page added

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,15 @@
+import { Header, Message, MessageInput, Avatar, BurgerMenu } from '../components';
+
+const Chat = () => {
+	return (
+		<div>
+			< Header />
+			<main>
+				
+				< Message text="latest message"/>
+			</main>
+			< Footer />
+
+		</div>
+	)};
+export default Chat;
