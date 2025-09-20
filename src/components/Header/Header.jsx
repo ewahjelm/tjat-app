063b3a49917ac8fab2e7d3195e2aa011c04c61d6
Ewa <ewahjelm84@gmail.com>
2025-09-20 23:42:54 +0200

    first try Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,11 @@
-const Header = () => {
-	return(
-		<>Header</>
-	)};
+import { Avatar } from "../../components";
+const Header = ({ friend = "NN" }) => {
+  return (
+    <header>
+      <Avatar>Default</Avatar>
+      <h1>Tjatapp</h1>
+      <h3>{friend}</h3>
+    </header>
+  );
+};
 export default Header;
