0f54a8c29fd61f65e19a3f2f4daf8c1bc0babfb0
Ewa <ewahjelm84@gmail.com>
2025-09-20 23:42:09 +0200

    added specific Avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Avatar = () => {
-
-	return(
-	<>Avatar</>
-	)
+const Avatar = ({
+  src = "https://i.pravatar.cc/150?img=24",
+  alt = "User avatar",
+  size = 68,
+}) => {
+  return (
+    <div style={{ width: size, height: size }}>
+      <img src={src} alt={alt} />
+    </div>
+  );
 };
 
 export default Avatar;
