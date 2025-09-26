505d2c71b1c36aa2651655901b18512653f599ac
Ewa <ewahjelm84@gmail.com>
2025-09-26 22:19:28 +0200

    Card css

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import styles from "./Card.module.css";
 
 const Card = () => {
-  return <>Card</>;
+  return (
+  <div className = {styles.wrapper} >
+  
+  </div>)
 };
 
 export default Card;
